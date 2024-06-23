85e17e24f04cc4ebb4140717e10563655ad9cc07
Kirill Markin <markinkirill@gmail.com>
2024-06-23 12:11:47 +0200

    logs format

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,8 +8,8 @@ import { UserData } from '../types';
 
 export const formatLogMessage = (ctx: MyContext, logMessage: string) => {
   const chat_id = ctx.chat?.id;
-  const username = ctx.from?.username || ctx.from?.id;
-  return `Chat: ${chat_id}, User: ${username}: ${logMessage}`;
+  const username = ctx.from?.username || "";
+  return `chat_id: ${chat_id}, user_id: ${ctx.from?.id}, username: ${username}: ${logMessage}`;
 }
 
 export async function fetchUserDataOrReplyWithError(ctx: MyContext): Promise<UserData> {
