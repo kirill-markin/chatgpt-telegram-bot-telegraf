52e316f20c6b77acc28adf12ffc6518da79684b3
Kirill Markin <markinkirill@gmail.com>
2024-12-30 20:05:44 +0100

    tests fix

--- __tests__/messageHandlers.test.ts
+++ __tests__/messageHandlers.test.ts
@@ -22,6 +22,34 @@ jest.mock('../src/database/database', () => ({
   }),
 }));
 
+jest.mock('fs', () => ({
+  readFileSync: jest.fn((path: string, encoding: string) => {
+    if (path === './temp/__temp_config.yaml') {
+      return `
+        gpt_model: 'gpt-4o'
+        gpt_model_for_image_url: 'gpt-4o'
+        strings:
+          reset_message: 'Old messages deleted'
+          no_openai_key_error: 'No OpenAI key provided. Please contact the bot owner.'
+          trial_ended_error: 'Trial period ended. Please contact the bot owner.'
+          trial_not_enabled_error: 'Trial period is not enabled. Please contact the bot owner.'
+          no_video_error: 'Bot can not process videos.'
+          no_answer_error: 'Bot can not answer to this message.'
+          help_string: 'This is a bot that can chat with you. You can ask it questions or just chat with it. The bot will try to respond to you as best as it can. If you want to reset the conversation, just type /reset.'
+          error_string: 'An error occurred. Please try again later.'
+        max_trials_tokens: 0
+        prompts:
+          - name: 'default'
+            text: 'This is the default prompt message.'
+      `;
+    }
+    return '';
+  }),
+  existsSync: jest.fn((path: string) => {
+    return path === '.env'; // Mock the existence of the .env file
+  }),
+}));
+
 describe('Message Handlers', () => {
   let mockContext: Partial<MyContext>;
 
