1a129c7bdafd54a410569a5c20ad692705f46c49
Kirill Markin <markinkirill@gmail.com>
2024-12-30 19:45:52 +0100

    tests setup

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    setupFiles: ['<rootDir>/jest.setup.js'],
     transform: {
         '^.+\\.ts?$': 'ts-jest',
     },
---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+process.env.DATABASE_URL = 'test-database-url';
+process.env.TELEGRAM_BOT_TOKEN = 'test-telegram-bot-token';
+process.env.OPENAI_API_KEY = 'test-openai-api-key';
